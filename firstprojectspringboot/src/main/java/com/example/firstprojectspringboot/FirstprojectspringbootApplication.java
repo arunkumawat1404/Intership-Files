d391261566b6927fa1347dd9c51e37ee5248f66c
arunkumawat1404 <mailto.arunkumawat@gmail.com>
2024-05-22 13:19:49 +0530

    Springboot

---
+++ firstprojectspringboot/src/main/java/com/example/firstprojectspringboot/FirstprojectspringbootApplication.java
@@ -0,0 +1,14 @@
+package com.example.firstprojectspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstprojectspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstprojectspringbootApplication.class, args);
+		System.out.println(" arun");
+	}
+
+}
