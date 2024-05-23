611e5b3b7db1caa7c42636e87081cf9b9d7ed737
arunkumawat1404 <mailto.arunkumawat@gmail.com>
2024-05-23 16:28:54 +0530

    Postmethod

---
+++ postmethod/src/main/java/com/example/postmethod/PostmethodApplication.java
@@ -0,0 +1,13 @@
+package com.example.postmethod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostmethodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostmethodApplication.class, args);
+	}
+
+}
