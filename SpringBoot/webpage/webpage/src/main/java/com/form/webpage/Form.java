2c8e918cef708a363cef7e2d4cfe72a649cc8422
arunkumawat1404 <mailto.arunkumawat@gmail.com>
2024-06-04 18:16:50 +0530

    Spring form

---
+++ SpringBoot/webpage/webpage/src/main/java/com/form/webpage/Form.java
@@ -0,0 +1,31 @@
+package com.form.webpage;
+
+public class Form {
+    private String name;
+    private String email;
+    private int phone;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public int getPhone() {
+        return phone;
+    }
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+    @Override
+    public String toString() {
+        return "Form [name=" + name + ", email=" + email + ", phone=" + phone + "]";
+    }
+    
+    
+}
---
+++ SpringBoot/webpage/webpage/src/main/java/com/form/webpage/FormController.java
@@ -0,0 +1,47 @@
+package com.form.webpage;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@RestController
+@RequestMapping("/api")
+public class FormController {
+
+   
+
+    @GetMapping("/form")
+    public String form(){
+        return "form";
+    }
+    
+    @PostMapping("/submit")
+    public String postMethodName(@RequestParam("name") String name,
+                                @RequestParam("email") String email,
+                                @RequestParam("phone")  int phone , 
+                                Model model) {
+
+        model.addAttribute("name" , name);
+        model.addAttribute("email" , email);
+        model.addAttribute("phone" , phone);
+
+        
+        return "redirect:/userDetails";
+    }
+    @GetMapping("/userDetails")
+    public String getMethod() {
+        return "userDetails";
+    }
+    
+    
+    
+
+    
+}
